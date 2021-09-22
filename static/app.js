3cddccc520ee5d3ea208d9d07cccbad69e3504c2
Tobi Dendom <tobidendom@pop-os.localdomain>
2021-09-22 12:14:27 +1000

    added Procfile

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
-// Display mobile menu 
+// Display mobile menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active');
